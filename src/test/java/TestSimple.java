2da64717a2f1997be65332ed7bfb3c2921902fc5
aashrutalmal <almal@wustl.edu>
2026-08-27 14:15:26 -0500

    update

--- src/test/java/TestSimple.java
+++ src/test/java/TestSimple.java
@@ -9,7 +9,7 @@ public class TestSimple {
 
     @Test
     public void testSimple() {
-        final int N = 247;
+        final int N = 69;
         Simple s = new Simple();
         s.setData(N);
         assertEquals("Value gotten from object does not match value put in", N, s.getData());
